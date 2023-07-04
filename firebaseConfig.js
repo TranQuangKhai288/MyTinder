4113614b780b64e476ac732b8ebd2b4f7e0e364f
griselda4774 <trandinhtam4774@gmail.com>
2023-07-04 15:31:07 +0700

    realtime database

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,10 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { getDatabase } from "firebase/database";
 
 const firebase = {
   apiKey: "AIzaSyCFYVFR8ptjmvUz7oQUKOhXpU9EHAvUQcs",
   authDomain: "mytinder-86073.firebaseapp.com",
+  databaseURL:
+    "https://mytinder-86073-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "mytinder-86073",
   storageBucket: "mytinder-86073.appspot.com",
   messagingSenderId: "247583145779",
@@ -17,3 +20,4 @@ export const FIREBASE_APP = initializeApp(firebase);
 export const FIRESTORE_DB = getFirestore(FIREBASE_APP);
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
 export const FIREBASE_STORAGE = getStorage(FIREBASE_APP);
+export const FIREBASE_REALTIME_DB = getDatabase(FIREBASE_APP);
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -14,17 +14,86 @@ import { WhiteGBIcon } from "../constants/icons";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { ScrollView } from "react-native-virtualized-view";
 import { useSelector } from "react-redux";
+import { TextInput } from "react-native";
+import { LIGHT_GRAY_COLOR, RED_COLOR } from "../constants/color";
+import { FIREBASE_REALTIME_DB } from "../../firebaseConfig";
+import { TextComponent } from "react-native";
+import { ref, set, push, onValue } from "@firebase/database";
 
 const ProfileScreen = () => {
   const insets = useSafeAreaInsets();
   const user = useSelector((state) => state.user.user);
-  console.log(user);
+  const [messageDetail, setMessageDetail] = useState("");
+  const [chatRoom, setChatRoom] = useState([]);
+  const sendMessage = (message, user) => {
+    const messageRef = ref(FIREBASE_REALTIME_DB, "messages");
+    const newChildRef = push(messageRef);
+    set(newChildRef, {
+      message: message,
+      uid: user.id,
+    });
+  };
+  useEffect(() => {
+    const messageRef = ref(FIREBASE_REALTIME_DB);
+    const handleNewMessage = (snapshot) => {
+      const message = snapshot.val();
+      if (message) {
+        console.log("message", message);
+        const messageArray = Object.values(message.messages).map(
+          ({ message, uid }) => ({ message, uid })
+        );
+        setChatRoom((prev) => messageArray);
+        console.log("Chatroom", chatRoom);
+      }
+    };
+
+    const subcrition = onValue(messageRef, handleNewMessage);
+
+    return () => {
+      subcrition();
+    };
+  }, []);
   return (
-    <View style={styles.container}>
-      <Image
-        source={{ uri: user.avatar.toString() }}
-        style={{ width: 300, height: 300 }}
+    <View
+      style={[
+        styles.container,
+        {
+          paddingTop: insets.top + 20,
+          paddingBottom: insets.bottom + 20,
+          paddingLeft: insets.left + 20,
+          paddingRight: insets.right + 20,
+        },
+      ]}
+    >
+      <TextInput
+        style={{
+          borderWidth: 1,
+          borderColor: LIGHT_GRAY_COLOR,
+          fontSize: 16,
+          padding: 4,
+        }}
+        onChangeText={(text) => {
+          setMessageDetail(text);
+        }}
       />
+      <TouchableOpacity
+        style={{
+          padding: 8,
+          backgroundColor: RED_COLOR,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        onPress={() => {
+          sendMessage(messageDetail, user);
+        }}
+      >
+        <Text style={{ color: "#FFFFFF" }}>Save</Text>
+      </TouchableOpacity>
+      {chatRoom.map((item, index) => (
+        <Text key={index} style={{ fontSize: 16, marginTop: 4, color: "#000" }}>
+          {item.message}
+        </Text>
+      ))}
     </View>
   );
 };
